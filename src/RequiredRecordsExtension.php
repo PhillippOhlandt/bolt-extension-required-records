d8c209df4d8d5f17875c6a329ef155a0bc267d66
Phillipp Ohlandt <phillipp.ohlandt@googlemail.com>
2016-11-08 10:37:57 +0100

    boostrap new extension

---
+++ src/RequiredRecordsExtension.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Bolt\Extension\Ohlandt\RequiredRecords;
+
+use Bolt\Extension\SimpleExtension;
+
+/**
+ * Required Records extension class.
+ *
+ * @author Phillipp Ohlandt <phillipp.ohlandt@googlemail.com>
+ */
+class RequiredRecordsExtension extends SimpleExtension
+{
+
+}
