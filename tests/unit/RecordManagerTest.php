4746cb9e78b8149dcafb0cae4f6154598f3207c2
Phillipp Ohlandt <phillipp.ohlandt@googlemail.com>
2016-11-09 10:38:57 +0100

    add tests for RecordManager class

---
+++ tests/unit/RecordManagerTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Bolt\Extension\Ohlandt\RequiredRecords\Tests;
+
+use Bolt\Extension\Ohlandt\RequiredRecords\Manager\RecordManager;
+use Bolt\Extension\Ohlandt\RequiredRecords\Record\RequiredRecord;
+use Bolt\Tests\BoltUnitTest;
+
+/**
+ * Field testing class.
+ *
+ * @author Phillipp Ohlandt <phillipp.ohlandt@googlemail.com>
+ */
+class RecordManagerTest extends BoltUnitTest
+{
+    /** @test */
+    public function it_parses_contenttypes_correctly()
+    {
+        $contenttypes = [
+            'pages' => [
+                'name' => 'Pages',
+                'singular_name' => 'Page',
+                'required' => [
+                    [
+                        'title' => 'About Us',
+                        'slug' => 'about-us'
+                    ]
+                ]
+            ],
+            'entries' => [
+                'name' => 'Entries',
+                'singular_name' => 'Entry'
+            ],
+            'social' => [
+                'name' => 'Social Media',
+                'singular_name' => 'Social Media',
+                'required' => [
+                    [
+                        'title' => 'Twitter',
+                        'slug' => 'twitter'
+                    ],
+                    [
+                        'title' => 'GitHub',
+                        'slug' => 'github'
+                    ]
+                ]
+            ]
+        ];
+
+        $manager = new RecordManager($contenttypes);
+
+        $this->assertCount(3, $manager->getRecords());
+        $this->assertContainsOnlyInstancesOf(RequiredRecord::class, $manager->getRecords());
+    }
+
+}
