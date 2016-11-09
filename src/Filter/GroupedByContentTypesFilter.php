e0af1c5c11bd505be3c8bfe48cf3211cb11a8159
Phillipp Ohlandt <phillipp.ohlandt@googlemail.com>
2016-11-09 12:23:06 +0100

    add GroupedByContentTypesFilter class plus tests

---
+++ src/Filter/GroupedByContentTypesFilter.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Bolt\Extension\Ohlandt\RequiredRecords\Filter;
+
+class GroupedByContentTypesFilter
+{
+    public static function filter(array $records)
+    {
+        $data = [];
+
+        foreach($records as $record) {
+            $data[$record->getContentType()][] = $record;
+        }
+
+        return $data;
+    }
+}
---
+++ tests/unit/GroupedByContentTypesFilterTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Bolt\Extension\Ohlandt\RequiredRecords\Tests;
+
+use Bolt\Extension\Ohlandt\RequiredRecords\Filter\GroupedByContentTypesFilter;
+use Bolt\Extension\Ohlandt\RequiredRecords\Manager\RecordManager;
+use Bolt\Tests\BoltUnitTest;
+
+/**
+ * Field testing class.
+ *
+ * @author Phillipp Ohlandt <phillipp.ohlandt@googlemail.com>
+ */
+class GroupedByContentTypesFilterTest extends BoltUnitTest
+{
+    /** @test */
+    public function it_groups_by_contenttype()
+    {
+        $contenttypes = [
+            'pages' => [
+                'name' => 'Pages',
+                'singular_name' => 'Page',
+                'required' => [
+                    [
+                        'title' => 'About Us',
+                        'slug' => 'about-us'
+                    ]
+                ]
+            ],
+            'entries' => [
+                'name' => 'Entries',
+                'singular_name' => 'Entry'
+            ],
+            'social' => [
+                'name' => 'Social Media',
+                'singular_name' => 'Social Media',
+                'required' => [
+                    [
+                        'title' => 'Twitter',
+                        'slug' => 'twitter'
+                    ],
+                    [
+                        'title' => 'GitHub',
+                        'slug' => 'github'
+                    ]
+                ]
+            ]
+        ];
+
+        $manager = new RecordManager($contenttypes);
+
+        $filteredRecords = GroupedByContentTypesFilter::filter($manager->getRecords());
+
+        $this->assertCount(2, $filteredRecords);
+        $this->assertCount(1, $filteredRecords['pages']);
+        $this->assertCount(2, $filteredRecords['social']);
+    }
+
+}
